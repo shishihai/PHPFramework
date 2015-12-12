13e84dd3d0945637c740a4a8925764ffa49988e1
shishihai <shishihai1989@163.com>
2015-12-12 18:17:37 +0800

    添加BaseController

--- controll/BaseController.php
+++ controll/BaseController.php
@@ -1,1 +1,46 @@
 <?php
+class BaseController{
+	public $module;
+	public $controller;
+	public $action;
+	public $aGPArr = array();
+	
+	public function __construct($module,$controller,$action){
+		
+		$this->module = $module;
+		$this->controller = $controller;
+		$this->action = $action;
+		
+		$this->aGPArr = array_merge($_GET,$_POST);
+		
+	}
+	
+	private function _initController(){
+		
+	}
+	
+	public function getVal($param,$type='string'){
+		if(is_array($param)){
+			foreach ($param as $key){
+				$this->CVal($key, $type);
+			}
+		}else{
+			$this->CVal($key, $type);
+		}
+	}
+	
+	public function CVal($key,$type){
+		switch ($type){
+			case 'string':
+				break;
+			case 'int':
+				break;
+			case 'bigint':
+				break;
+			case 'none':
+				break;
+			default:
+				break;
+		}
+	}
+}
